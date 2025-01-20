ffeb54214501f5ea09f77ad68fbbc84d40b87a83
haroldmacmillan <haroldmacmillan@localhost>
2025-01-19 23:29:40 -0500

    Merge origin/master
    # Conflicts:
    #	.gitignore
    #	src/wordsearchstudent/WordSearchSolver.java

---
+++ JavaApplication6/src/javaapplication6/FinalProject.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication6;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+/**
+ *
+ * @author haroldmacmillan
+ */
+public class FinalProject {
+    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame ("AlchemyCraft");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize (1000, 1000);
+        projectPanel project = new projectPanel();
+        frame.add(project);
+        frame.setVisible(true);
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+
+    
+}
